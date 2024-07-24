01402d196cf2183b43258350436ffea66d2633a1
Alvin Alic <alvin.alic@gmail.com>
2024-07-24 17:14:32 -0500

    Add overflow hidden to buttons

--- javascript.js
+++ javascript.js
@@ -42,20 +42,4 @@ console.log(operate(1, "*", 100));
 console.log(operate(50, "-", 5));
 console.log(operate(0, "-", 5));
 
-const display = document.querySelector('.display');
 
-function updateDisplay(){
-    const display = document.querySelector('.display');
-    
-}
-
-function buttonlisteners(){
-
-}
-
-const one = document.querySelector('#1');
-one.addEventListener('click', () =>{
-    const display = document.getElementById('1');
-    let ran = document.createTextNode(1)
-    display.value = 1;
-})
