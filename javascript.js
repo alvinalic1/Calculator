aa07e3820fd98588133abe58fcfec687b829d607
Alvin Alic <alvin.alic@gmail.com>
2024-08-01 12:13:11 -0500

    Solve simple 2+2=4 equation

--- javascript.js
+++ javascript.js
@@ -22,7 +22,10 @@ const operatorObject ={
 }
 
 
-let number1, operator, number2;
+let number1 = 0;
+let operator;
+let number2 = 0;
+let operatorclicked = false;
 
 const operate = function(number1, operator, number2){
 
@@ -43,12 +46,64 @@ console.log(operate(50, "-", 5));
 console.log(operate(0, "-", 5));
 
 let one = document.getElementById("1");
+let two = document.getElementById("2");
+let three = document.getElementById("3");
+let four = document.getElementById("four");
+let five = document.getElementById("five");
+let six = document.getElementById("six");
+let seven = document.getElementById("seven");
+let eight = document.getElementById("eight");
+let nine = document.getElementById("nine");
+let zero = document.getElementById("zero");
+
+let clear = document.getElementById("clear");
+let pos_neg = document.getElementById("pos-neg");
+let percent = document.getElementById("percentage");
+let division = document.getElementById("divide");
+let multiplication = document.getElementById("multiply");
+let subtraction = document.getElementById("subtract");
+let addition = document.getElementById("add");
+let decimal = document.getElementById("decimal");
+let equals = document.getElementById("equals");
+
 let largeDisplay = document.getElementById("Value");
 let miniDisplay = document.getElementById("mini-display")
+
+
 one.addEventListener("click", () =>{
-    let number = 1;
-    largeDisplay.value += largeDisplay.value = number;
-    miniDisplay.value = largeDisplay.value;
-    let  = largeDisplay.value;
+    let number = "1";
+    largeDisplay.value += number;
+    miniDisplay.value += number;
+    if(operatorclicked === false){
+        number1 += number;
+    }else{
+        number2 += number;
+    }
 })
+
+addition.addEventListener("click", () =>{
+    operator = "+";
+    operatorclicked = true;
+    miniDisplay.value += operator
+    largeDisplay.value='';
+})
+
+two.addEventListener("click", () =>{
+    let number = "2";
+    largeDisplay.value += number;
+    miniDisplay.value += number;
+    if(operatorclicked === false){
+        number1 += number;
+    }else{
+        number2 += number;
+    }
+})
+
+equals.addEventListener("click", () =>{
+    console.log(number1);
+    console.log(operator);
+    console.log(number2);
+    largeDisplay.value = operate(Number(number1), operator, Number(number2));
+})
+
 
